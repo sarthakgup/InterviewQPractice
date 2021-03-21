e9ab232b36cb5984af23391becf7b60841a21c4f
sarthakgup <16374350+sarthakgup@users.noreply.github.com>
2021-03-21 14:15:59 -0700

    workflow update

--- findDuplicates.java
+++ findDuplicates.java
@@ -1,13 +1,18 @@
 public class findDuplicates
 {
+    
+
     public static void main(String[] args)
     {
+        int[] input = {1,2,1,3,1,2,3};       
+        System.out.println("got: " + dupeFinder(input) +  ",  Expected: 3");
 
         int[] input2 = {0};       
         System.out.println("got: " + dupeFinder(input2) +  ",  Expected: 0");
 
         int[] input3 = {0,0,1,1,2,2,3,3,3};       
-        System.out.println("got: " + dupeFinder(input3) +  ",  Expected: 3");
+        System.out.println("got: " + dupeFinder(input3) +  ",  Expected: 4");
+        assert dupeFinder(input3) == 4 : "expected 4";
 
         int[] input4 = {1,2,3,4};       
         System.out.println("got: " + dupeFinder(input4) +  ",  Expected: 0");
@@ -15,11 +20,36 @@ public class findDuplicates
         int[] input5 = { };       
         System.out.println("got: " + dupeFinder(input5) +  ",  Expected: 0");
 
+        int[] input6 = {0,1,2,3,0};       
+        System.out.println("got: " + dupeFinder(input6) +  ",  Expected: 1");
+
     }
 
     public static int dupeFinder(int[] inputVals)
     {
-        int total = 0;
+        int totalDupes = 0;
+
+        for(int i = 0; i < inputVals.length; i++)
+        {
+            int current = inputVals[i];
+
+            for(int j = i+1; j < inputVals.length;j++)
+            {
+                if(inputVals[j] == current)
+                {
+                    totalDupes++;
+                    break;
+                }
+            }
+        }
+
+        return totalDupes;
+    }
+
+
+
+        /*
+        int totalDupes = 0;
 
         for(int a = 0; a < inputVals.length; a++)
         {
@@ -47,17 +77,17 @@ public class findDuplicates
             //occurrences--;
 
             //divide by two for duplicates
-            total += occurrences/2;
+            totalDupes += occurrences/2;
 
         }
 
-        return total;
+        return totalDupes;
     }
 
 
 
 
-        /*
+        
 
         int counter = 0;
         int currentVal = inputVals[0];
---
+++ sortingPractice.java
@@ -0,0 +1,30 @@
+public class sortingPractice
+{
+    public static void main(String[] args)
+    {
+        int[] input = {1,2,4,5,3,6,4,7,5};
+    }
+
+    public static int[] bubbleSort(int[] inputArr)
+    {
+        int[] temp = new int[inputArr.length];
+
+
+        return temp; 
+    }
+
+    public static void swap(int[] inputArr, int index1, int index2)
+    {
+
+    }
+
+    /*
+    public static int[] quickSort(int[] inputArr)
+    {
+        
+    }
+    */
+    
+
+
+}
