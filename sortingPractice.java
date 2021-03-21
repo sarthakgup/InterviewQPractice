eaa82cdee9ea0789d96af3226287c45217cdb598
sarthakgup <16374350+sarthakgup@users.noreply.github.com>
2021-03-21 14:42:58 -0700

    worked on bubble sorts

--- sortingPractice.java
+++ sortingPractice.java
@@ -3,11 +3,40 @@ public class sortingPractice
     public static void main(String[] args)
     {
         int[] input = {1,2,4,5,3,6,4,7,5};
+        int[] output = new int[input.length];
+        output = bubbleSort(input);
+
+        //printing output
+        for(int i = 0; i < input.length; i++)
+        {
+            System.out.println(output[i]);
+        }
     }
 
     public static int[] bubbleSort(int[] inputArr)
     {
-        int[] temp = new int[inputArr.length];
+        int[] original = inputArr;
+        int[] temp = inputArr;
+        boolean done = false; //boolean for if bubble sort has completed
+
+        while(done != true)
+        {
+            original = inputArr;
+
+            for(int i = 0; i < temp.length-1; i++)
+            {
+                if(temp[i+1] < temp[i])
+                {
+                    swap(temp, temp[i+1], temp[i]);
+                }
+            }
+
+            //if no change made from original in entire pass, bubble is done
+            if(original == temp)
+            {
+                done = true;
+            }
+        }
 
 
         return temp; 
@@ -15,7 +44,9 @@ public class sortingPractice
 
     public static void swap(int[] inputArr, int index1, int index2)
     {
-
+        int temp = inputArr[index1];
+        inputArr[index1] = inputArr[index2];
+        inputArr[index2] = temp;
     }
 
     /*
